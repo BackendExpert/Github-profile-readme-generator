a7c2b033647a6d3c909b693662e43cb810b8e352
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2025-01-22 18:06:14 +0530

    Update PlayGround.jsx

--- src/pages/PlayGround/PlayGround.jsx
+++ src/pages/PlayGround/PlayGround.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 
-
-
 const PlayGround = () => {
   return (
-    <div>PlayGround</div>
+    <div className='pt-24 px-24'>
+        <div className="flex">
+            <div className="w-1/4 mr-2">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum sit optio itaque autem beatae dolorem temporibus nam soluta explicabo culpa, eum, at officiis iure accusantium accusamus dolores facilis maxime quas!
+            </div>
+            <div className="w-full ml-2">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum libero saepe veritatis modi asperiores. Saepe quas amet est, minima eum expedita aliquid porro ratione officiis nobis unde ad corrupti praesentium?
+            </div>
+        </div>
+    </div>
   )
 }
 
